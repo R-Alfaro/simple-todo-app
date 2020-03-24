2ec9f7e33be6beb552f063a1753c65d23b89913b
Ramiro Alfaro <rkmhr.alfaro@gmail.com>
2020-03-23 23:04:46 -0700

    Updates

---
+++ precache-manifest.2c5278a4f92a3151d342ac9058046f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950b9df6afdb4e406eb0846f6bef379b",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "f12e4ea3b5329c1a2584",
+    "url": "/simple-todo-app/static/css/main.74a9892d.chunk.css"
+  },
+  {
+    "revision": "d3836c0b4cbd2dd04969",
+    "url": "/simple-todo-app/static/js/2.cce0aaca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.cce0aaca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12e4ea3b5329c1a2584",
+    "url": "/simple-todo-app/static/js/main.4a21465d.chunk.js"
+  },
+  {
+    "revision": "d3f8aceb317e5aa1253f",
+    "url": "/simple-todo-app/static/js/runtime-main.6f5ce6f0.js"
+  }
+]);
